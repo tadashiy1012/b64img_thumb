c39e257cdcf25994646b0c8cfd6e63929c936ca3
tadashiy1012 <tadashi.y1012@gmail.com>
2016-06-17 18:18:02 +0900

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,70 @@
 module.exports = (function() {
   "use strict";
-  return function b64imgThumb() {
+  const fs = require('fs');
+  const path = require('path');
+  const resizeImg = require('resize-img');
+  const ALL_EXT = '*';
+  const getFileNames = function(tgtDirPath) {
     return new Promise((resolve, reject) => {
-      resolve(true);
+      fs.readdir(tgtDirPath, (err, data) => {
+        if (err) { reject(err); }
+        else { resolve(data); }
+      })
+    });
+  };
+  const checkStat = function(tgtFilePath) {
+    return new Promise((resolve, reject) => {
+      fs.stat(tgtFilePath, (err, stats) => {
+        if (err) { reject(err); }
+        else { resolve([tgtFilePath, stats.isFile()]); }
+      });
+    });
+  };
+  const getFileExt = function(tgtFilePath) {
+    return tgtFilePath.substring(tgtFilePath.lastIndexOf('.') + 1);
+  };
+  const getBase64 = function(tgtFilePath) {
+    return new Promise((resolve, reject) => {
+      resizeImg(
+        fs.readFileSync(tgtFilePath),
+        {width: 128, height: 128}
+      ).then((buf) => {
+        const ext = getFileExt(tgtFilePath);
+        const head = 'data:image/' + ext + ';base64,';
+        const b64 = head + buf.toString('base64');
+        resolve(b64);
+      }).catch((err) => {
+        if (err.message === 'Image format not supported') {
+          resolve();
+        } else {
+          reject(err);
+        }
+      });
+    });
+  };
+  return function b64imgThumb(tgtDirectory, tgtExt = ALL_EXT) {
+    return new Promise((resolve, reject) => {
+      getFileNames(tgtDirectory).then((data) => {
+        const pAry = [];
+        data.forEach((file) => {
+          if (file.substring(file.lastIndexOf('.') + 1) === tgtExt || tgtExt == ALL_EXT) {
+            pAry.push(checkStat(path.join(tgtDirectory, file)));
+          }
+        });
+        return Promise.all(pAry);
+      }).then((arg) => {
+        const files = arg.filter(a => a[1]).map(a => a[0]);
+        const pAry = [];
+        files.forEach((file) => {
+          pAry.push(getBase64(file));
+        });
+        return Promise.all(pAry);
+      }).then((arg) => {
+        resolve(arg.filter(a => a !== undefined));
+      }).catch((err) => {
+        console.log(err);
+        reject(err);
+      });
     });
   };
 })();
--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
+const path = require('path');
 const assert = require('power-assert');
 const thumb = require('../index.js');
 
+const testPicDir = path.join(__dirname, 'test_image');
+
 describe('b64img_thumb test', () => {
   it('test1', (done) => {
-    thumb().then((result) => {
-      assert(result === true);
+    thumb(testPicDir).then((result) => {
+      assert(result !== undefined);
       done();
     });
   })
