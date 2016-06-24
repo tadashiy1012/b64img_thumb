fcffc19c54fa5ab32db8a53369582688fb684d8a
tadashiy1012 <tadashi.y1012@gmail.com>
2016-06-24 16:47:28 +0900

    3rd commit

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ module.exports = (function() {
   const getFileExt = function(tgtFilePath) {
     return tgtFilePath.substring(tgtFilePath.lastIndexOf('.') + 1);
   };
-  const getBase64 = function(tgtFilePath) {
+  const getBase64 = function(tgtFilePath, width, height) {
     return new Promise((resolve, reject) => {
       resizeImg(
         fs.readFileSync(tgtFilePath),
-        {width: 128, height: 128}
+        {width: width, height: height}
       ).then((buf) => {
         const ext = getFileExt(tgtFilePath);
         const head = 'data:image/' + ext + ';base64,';
@@ -42,12 +42,15 @@ module.exports = (function() {
       });
     });
   };
-  return function b64imgThumb(tgtDirectory, tgtExt = ALL_EXT) {
+  return function b64imgThumb(tgtDirectory, options = {
+     ext: ALL_EXT, width: 100, height: 100
+  }) {
     return new Promise((resolve, reject) => {
       getFileNames(tgtDirectory).then((data) => {
         const pAry = [];
         data.forEach((file) => {
-          if (file.substring(file.lastIndexOf('.') + 1) === tgtExt || tgtExt == ALL_EXT) {
+          if (file.substring(file.lastIndexOf('.') + 1) === options.ext 
+          || options.ext === ALL_EXT) {
             pAry.push(checkStat(path.join(tgtDirectory, file)));
           }
         });
@@ -56,13 +59,12 @@ module.exports = (function() {
         const files = arg.filter(a => a[1]).map(a => a[0]);
         const pAry = [];
         files.forEach((file) => {
-          pAry.push(getBase64(file));
+          pAry.push(getBase64(file, options.width, options.height));
         });
         return Promise.all(pAry);
       }).then((arg) => {
         resolve(arg.filter(a => a !== undefined));
       }).catch((err) => {
-        console.log(err);
         reject(err);
       });
     });
--- test/test.js
+++ test/test.js
@@ -10,5 +10,22 @@ describe('b64img_thumb test', () => {
       assert(result !== undefined);
       done();
     });
-  })
+  });
+  it('test2', (done) => {
+    const opt = {
+      ext: 'jpg',
+      width: 50,
+      height: 50
+    };
+    thumb(testPicDir, opt).then((resp) => {
+      assert(resp !== undefined);
+      done();
+    });
+  });
+  it('test3', (done) => {
+    thumb().catch((err) => {
+      assert(err !== undefined);
+      done();
+    });
+  });
 });
